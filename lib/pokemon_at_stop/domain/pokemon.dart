7de48c181e62578e5713fa48a3b17ca0d9dd237a
giboin <louis.giboin@imt-atlantique.net>
2023-01-03 17:09:37 +0100

    pokemon tojson

--- lib/pokemon_at_stop/domain/pokemon.dart
+++ lib/pokemon_at_stop/domain/pokemon.dart
@@ -2,10 +2,24 @@ class Pokemon {
   String name;
   String pictureUrl;
   int level;
+  int? pokedexId;
 
   Pokemon({
     required this.name,
     required this.level,
     required this.pictureUrl,
+    this.pokedexId,
   });
+
+  String toJson() {
+    return '{'
+        '"pokemon": {'
+            '"name": "$name",'
+            '"pictureUrl": "$pictureUrl",'
+            '"level": "$level",'
+            '"pokedexId": "$pokedexId"'
+        '}'
+    '}';
+  }
+
 }
