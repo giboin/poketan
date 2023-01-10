51d8b332dee6d45d1da6197af250fc3fc545a93b
clementDours <clement.dours98@gmail.com>
2023-01-10 15:44:22 +0100

    commit before fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hackathon/home_page/controller/bloc/home_bloc.dart';
 import 'package:hackathon/home_page/controller/view/home_view.dart';
 import 'package:hackathon/pokemon_at_stop/presentation/controller/at_stop_bloc.dart';
 import 'package:hackathon/pokemon_at_stop/presentation/at_stop_view.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String temporaryPath = (await getTemporaryDirectory()).absolute.path;
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: Directory("${temporaryPath}/hydrated_bloc_storage"));
   runApp(const MyApp());
 }
 
@@ -18,8 +26,6 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
         providers: [
           BlocProvider<HomeBloc>(create: ((context) => HomeBloc())),
-          BlocProvider<AtStopBloc>(
-              lazy: false, create: (context) => AtStopBloc()),
         ],
         child: MaterialApp(
           title: 'Flutter Demo',
@@ -29,7 +35,18 @@ class MyApp extends StatelessWidget {
           ),
           routes: {
             '/': (context) => const HomeView(),
-            'pokemon_at_stop': (context) => const PokemonsAtStopView(),
+            'pokemon_at_stop': (context) => BlocProvider(
+                create: (context) => AtStopBloc(
+                      stopName: context
+                          .read<HomeBloc>()
+                          .state
+                          .responseJson!["pokemon_data"],
+                      wildPokemon: context
+                          .read<HomeBloc>()
+                          .state
+                          .responseJson!["stop_name"],
+                    ),
+                child: const PokemonsAtStopView()),
           },
         ));
   }
--- lib/pokemon_at_stop/presentation/controller/at_stop_bloc.dart
+++ lib/pokemon_at_stop/presentation/controller/at_stop_bloc.dart
@@ -5,21 +5,18 @@ import 'package:equatable/equatable.dart';
 
 import 'package:hackathon/pokemon_at_stop/domain/pokemon.dart';
 import 'package:http/http.dart' as http;
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'at_stop_event.dart';
 part 'at_stop_state.dart';
 
-class AtStopBloc extends Bloc<AtStopEvent, AtStopState> {
+class AtStopBloc extends HydratedBloc<AtStopEvent, AtStopState> {
   final String serverUrl = "https://hackathon-server.osc-fr1.scalingo.io/";
 
-  AtStopBloc()
+  AtStopBloc({required String stopName, required Pokemon wildPokemon})
       : super(AtStopInitialState(
-            stopName: "Chantrerie - Grandes Ecoles",
-            wildPokemon: Pokemon(
-                name: "Ecremeuh",
-                level: 20,
-                pictureUrl:
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/241.png"),
+            stopName: stopName,
+            wildPokemon: wildPokemon,
             pokelist: [
               Pokemon(
                   name: "Salamouche",
@@ -65,5 +62,55 @@ class AtStopBloc extends Bloc<AtStopEvent, AtStopState> {
           chosenPokemon: event.pokemon,
           winner: winnerBool));
     });
+  }
+
+  @override
+  AtStopState? fromJson(Map<String, dynamic> json) {
+    return AtStopInitialState(
+        pokelist: [
+          Pokemon(
+              name: "Salamouche",
+              level: 2,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"),
+          Pokemon(
+              name: "Bulbazar",
+              level: 50,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"),
+          Pokemon(
+              name: "Carapute",
+              level: 37,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"),
+          Pokemon(
+              name: "Doudoudou",
+              level: 5,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png"),
+          Pokemon(
+              name: "Sablaire",
+              level: 31,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/27.png"),
+          Pokemon(
+              name: "Ratatrouille",
+              level: 22,
+              pictureUrl:
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"),
+        ],
+        stopName: "hydrated memory",
+        wildPokemon: Pokemon(
+            level: 1,
+            name: "Ratatrouille",
+            pictureUrl:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"));
+  }
+
+  @override
+  Map<String, dynamic>? toJson(AtStopState state) {
+    return {
+      'pokelist': state.pokelist.map((e) => e.toJson()),
+    };
   }
 }
