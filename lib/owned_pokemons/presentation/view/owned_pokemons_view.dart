49d344e6a24abea545faa991f4cd4841853b55e2
gaelHerce <gael.herce@gmail.com>
2023-01-17 16:13:50 +0100

    Choix des starters

--- lib/owned_pokemons/presentation/view/owned_pokemons_view.dart
+++ lib/owned_pokemons/presentation/view/owned_pokemons_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hackathon/owned_pokemons/presentation/controller/owned_pokemons_bloc.dart';
 import 'package:hackathon/owned_pokemons/presentation/view/helpers/inventory_pokemon_card.dart';
+import 'package:hackathon/owned_pokemons/presentation/view/helpers/pick_starter_button.dart';
 import 'package:hackathon/pokemon_at_stop/domain/pokemon.dart';
 
 class OwnedPokemons extends StatelessWidget {
@@ -9,55 +10,113 @@ class OwnedPokemons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     List<Pokemon> myPokemons = context.read<OwnedPokemonsBloc>().state.pokeList;
+    List<Pokemon> starters = [
+      Pokemon(
+          name: "Bulbizarre",
+          level: 5,
+          pictureUrl:
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+          pokedexId: 1),
+      Pokemon(
+          name: "Salamèche",
+          level: 5,
+          pictureUrl:
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+          pokedexId: 4),
+      Pokemon(
+          name: "Carapuce",
+          level: 5,
+          pictureUrl:
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+          pokedexId: 7),
+    ];
     //List<Pokemon> myTeam = context.read<OwnedPokemonsBloc>().state.pokeTeam;
 
-
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pop(context),
-        child: const Icon(Icons.home),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage("assets/ronflex_background.png"),
+    return BlocBuilder<OwnedPokemonsBloc, OwnedPokemonsState>(builder: (context, state) {
+      if (state is OwnedPokemonsInitial){
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Icon(Icons.home),
           ),
-        ),
-        child: BlocConsumer<OwnedPokemonsBloc, OwnedPokemonsState>(
-          listener: (context, state) {
-            myPokemons = state.pokeList;
-            //myTeam = state.pokeTeam;
-
-          },
-          builder: (context, state) {
-            return SafeArea(
-            child: Column(children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 8.0, bottom: 10.0),
-                child: Text(
-                  "Mes Pokemouns",
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage("assets/ronflex_background.png"),
               ),
-              Expanded(
-                child: GridView.builder(
-                    itemCount: myPokemons.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
+            ),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 8.0, bottom: 10.0),
+                    child: Text(
+                      "Choisis ton premier pokémon",
+                      style: TextStyle(fontSize: 25, color: Colors.black),
                     ),
-                    itemBuilder: (ctx, int index) {
-                      return InventoryPokemonCard(pokemon: myPokemons[index]);
-                    }),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: starters.length,
+                        itemBuilder: (ctx, int index) {
+                          return PickStarterButton(pokemon: starters[index]);
+                        }),
+                  ),
+                ],
               ),
-            ]),
-          );
-          },
-        ),
-      ),
-    );
+            ),
+          ),
+        );
+      }
+      else {
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Icon(Icons.home),
+          ),
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage("assets/ronflex_background.png"),
+              ),
+            ),
+            child: BlocConsumer<OwnedPokemonsBloc, OwnedPokemonsState>(
+              listener: (context, state) {
+                myPokemons = state.pokeList;
+                //myTeam = state.pokeTeam;
+              },
+              builder: (context, state) {
+                return SafeArea(
+                  child: Column(children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 10.0),
+                      child: Text(
+                        "Mes Pokemouns",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      ),
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                          itemCount: myPokemons.length,
+                          gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemBuilder: (ctx, int index) {
+                            return InventoryPokemonCard(
+                                pokemon: myPokemons[index]);
+                          }),
+                    ),
+                  ]),
+                );
+              },
+            ),
+          ),
+        );
+      }
+    });
   }
 }
